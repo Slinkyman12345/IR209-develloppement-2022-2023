1ea537443ca39225100549e3646f2b7d9ed48b4f
Slinkyman12345 <78739902+Slinkyman12345@users.noreply.github.com>
2023-07-13 15:26:29 +0200

    Add files via upload

--- code coté serveur.c
+++ code coté serveur.c
@@ -17,8 +17,8 @@ void executeCommand(const char *command, char *response) {
     // Exécution de la commande et récupération de la sortie
     fp = popen(command, "r");
     if (fp == NULL) {
-        perror("Erreur lors de l'exécution de la commande");
-        exit(1);
+        snprintf(response, BUFFER_SIZE, "Erreur lors de l'exécution de la commande : %s\n", strerror(errno));
+        return;
     }
 
     // Lecture du résultat de la commande
@@ -29,14 +29,19 @@ void executeCommand(const char *command, char *response) {
     // Fermeture du flux
     status = pclose(fp);
     if (status == -1) {
-        perror("Erreur lors de la fermeture du flux");
-        exit(1);
+        snprintf(response, BUFFER_SIZE, "Erreur lors de la fermeture du flux : %s\n", strerror(errno));
     }
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    // Vérifier si le programme est exécuté avec les privilèges de root
+    if (getuid() != 0) {
+        printf("Ce programme doit être exécuté avec les privilèges de root.\n");
         exit(1);
     }
 
@@ -48,7 +53,7 @@ int main(int argc, char *argv[]) {
     // Créer une socket
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0) {
-        perror("Erreur lors de la création de la socket");
+        printf("Erreur lors de la création de la socket : %s\n", strerror(errno));
         exit(1);
     }
 
@@ -57,15 +62,22 @@ int main(int argc, char *argv[]) {
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(atoi(argv[1]));
 
+    // Réutiliser l'adresse et le port même en cas de déconnexion brutale
+    int option = 1;
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option)) < 0) {
+        printf("Erreur lors de la configuration de la socket : %s\n", strerror(errno));
+        exit(1);
+    }
+
     // Lier la socket à l'adresse IP et au port
     if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
-        perror("Erreur lors du bind");
+        printf("Erreur lors du bind : %s\n", strerror(errno));
         exit(1);
     }
 
     // Mettre la socket en mode écoute
     if (listen(serverSocket, 1) < 0) {
-        perror("Erreur lors de la mise en écoute");
+        printf("Erreur lors de la mise en écoute : %s\n", strerror(errno));
         exit(1);
     }
 
@@ -75,13 +87,15 @@ int main(int argc, char *argv[]) {
     socklen_t clientAddressLength = sizeof(clientAddress);
     clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
     if (clientSocket < 0) {
-        perror("Erreur lors de l'acceptation de la connexion");
+        printf("Erreur lors de l'acceptation de la connexion : %s\n", strerror(errno));
         exit(1);
     }
 
     printf("Connexion établie avec le client %s:%d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
 
-    while (1) {
+    int stopReceived = 0;
+
+    while (!stopReceived) {
         // Réinitialiser les buffers
         memset(command, 0, sizeof(command));
         memset(response, 0, sizeof(response));
@@ -89,24 +103,35 @@ int main(int argc, char *argv[]) {
         // Recevoir la commande du client
         ssize_t bytesRead = recv(clientSocket, command, BUFFER_SIZE - 1, 0);
         if (bytesRead < 0) {
-            perror("Erreur lors de la réception des données");
+            printf("Erreur lors de la réception des données : %s\n", strerror(errno));
             exit(1);
         }
 
-        // Exécuter la commande
-        executeCommand(command, response);
+        // Vérifier si la commande est "stop"
+        if (strcmp(command, "stop\n") == 0) {
+            stopReceived = 1;
+        } else {
+            // Exécuter la commande en tant que root
+            executeCommand(command, response);
 
-        // Envoyer la réponse au client
-        ssize_t bytesSent = send(clientSocket, response, strlen(response), 0);
-        if (bytesSent < 0) {
-            perror("Erreur lors de l'envoi des données");
-            exit(1);
+            // Envoyer la réponse au client
+            ssize_t bytesSent = send(clientSocket, response, strlen(response), 0);
+            if (bytesSent < 0) {
+                printf("Erreur lors de l'envoi des données : %s\n", strerror(errno));
+                exit(1);
+            }
         }
     }
 
-    // Fermer les sockets
-    close(clientSocket);
-    close(serverSocket);
+    // Fermer la connexion client
+    if (close(clientSocket) < 0) {
+        printf("Erreur lors de la fermeture de la connexion client : %s\n", strerror(errno));
+    }
+
+    // Fermer la socket du serveur
+    if (close(serverSocket) < 0) {
+        printf("Erreur lors de la fermeture de la socket du serveur : %s\n", strerror(errno));
+    }
 
     return 0;
 }
